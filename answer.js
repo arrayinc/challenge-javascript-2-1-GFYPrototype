68bcca58d648a54a41ec7bc511ca5e834cc9aef2
jwelchman <j.welchman@yahoo.com>
2019-11-20 15:49:23 -0700

    finished challenge 1. never thought i'd see high school math again.

--- answer.js
+++ answer.js
@@ -1,1 +1,12 @@
 // *** YOUR ANSWER BELOW ***
+
+function equation() {
+    const start = "f(x) = ";
+    let valueA = "x²";
+    let operator = "+";
+    let valueB = "5x";
+    let valueC = 6;
+    return start + valueA + " " + operator + " " + valueB + " " + operator + " " + valueC;
+}
+
+console.log(equation());
